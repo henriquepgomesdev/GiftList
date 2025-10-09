ed6224faccd3816acebe6a91c63b3a72a952a525
henriquegomesdev <39200841+henriquegomesdev@users.noreply.github.com>
2025-10-09 10:50:56 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/giftlist/GiftListApplication.java
@@ -0,0 +1,11 @@
+package com.giftlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GiftListApplication.class, args);
+    }
+}
